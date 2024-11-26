d85fc1471dd0a5c9d1f10b9c7197dc3ca3189266
tranductinh2001 <0352911750a@gmail.com>
2024-11-26 22:26:13 +0700

    Replace all content in develop branch with updated version

--- He_Thong_Ban_Hang-BE/src/main/java/com/example/demo/DTO/ImageDTO.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.DTO;
-
-public class ImageDTO {
-    private long id;
-    private String name;
-    private String type;
-    private long size;
-    private String uploadedBy;
-}
--- He_Thong_Ban_Hang-BE/src/main/java/com/example/demo/entity/OrderStatus.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.entity;
-
-public enum OrderStatus {
-    PENDING,
-    COMPLETED,
-    SHIPPING,
-    PAID,
-    CANCELED
-}
